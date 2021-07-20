54f1fa3c7d0c349704845a53cbbf5a45442ec7aa
Caily Tuan <tuan@caily.com.vn>
2021-07-20 17:55:54 +0700

    edit html

---
+++ xxx/js/scripts.js
@@ -0,0 +1,250 @@
+/* -----------------------------------------------
+Table of Contents (common js)
+--------------------------------------------------
+1. userAgent判別
+2. URL判別
+3. 設定
+4. JSファイル読み込み時即実行(Execute JavaScript when the page loaded.)
+5. DOM構築後実行(Execute JavaScript when the DOM is fully loaded.)
+6. 画像など含めてページ読込み完了後に実行(Execute JavaScript when the Window Object is fully loaded.)
+7. 動的コンテンツに対してイベントを設定
+8. その他のイベントを設定
+9. 関数(プラグイン)複数
+
+// require jQuery JavaScript Library v3.5.1
+/* ------------------------------------------------------------
+ * [ userAgent ] http://www.useragentstring.com/pages/useragentstring.php
+ * ------------------------------------------------------------ */
+var ua                   = window.navigator.userAgent;
+var appVer               = window.navigator.appVersion;
+
+//PC
+var isIE                 = ua.indexOf('MSIE') != -1 || ua.indexOf('Trident') != -1;
+var isIE6                = isIE && appVer.indexOf('MSIE 6') != -1;
+var isIE7                = isIE && appVer.indexOf('MSIE 7.') != -1;
+var isIE8                = isIE && ua.indexOf('Trident/4.') != -1;
+var isIE9                = isIE && ua.indexOf('Trident/5.') != -1;
+var isIE10               = isIE && ua.indexOf('Trident/6.') != -1;
+var isIE11               = ua.indexOf('Trident/7.') != -1;
+
+var isFirefox            = ua.indexOf('Firefox') != -1;
+var isChrome             = ua.indexOf('Chrome') != -1;
+var isSafari             = ua.indexOf('Safari') != -1;
+
+//Mobile (smartphone + tablet)
+var isMobileSafari       = ua.match(/iPhone|iPad|iPod/i) ? true : false;
+var isMobileSafariTypeT  = ua.match(/ipad/i) ? true : false;
+var isMobileSafariTypeS  = ua.match(/iphone|ipod/i) ? true : false;
+var isAndroid            = ua.indexOf('Android') != -1;
+var isMobileAndroidTypeT = isAndroid && ua.indexOf('Mobile') == -1;
+var isMobileAndroidTypeS = isAndroid && ua.indexOf('Mobile') != -1;
+var isAndroidChrome      = isChrome && isAndroid;
+var isAndroidFirefox     = isFirefox && isAndroid;
+var isMobileFirefox      = isFirefox && ua.indexOf('Mobile') != -1;
+var isTabletFirefox      = isFirefox && ua.indexOf('Tablet') != -1;
+
+//PC or Mobile
+var isTablet             = isMobileSafariTypeT || isMobileAndroidTypeT || isTabletFirefox;
+var isSmartPhone         = isMobileSafariTypeS || isMobileAndroidTypeS || isMobileFirefox;
+var isMobile             = isTablet || isSmartPhone || isAndroidChrome || isAndroidFirefox;
+var isPC                 = !isMobile;
+
+
+
+/* ------------------------------------------------------------
+ * [ Location ]
+ * ------------------------------------------------------------ */
+var  locationHref     = window.location.href,     // http://www.google.com:80/search?q=demo#test
+     locationProtocol = window.location.protocol, // http:
+     locationHostname = window.location.hostname, // www.google.com
+     locationHost     = window.location.host,     // www.google.com:80
+     locationPort     = window.location.port,     // 80
+     locationPath     = window.location.pathname, // /search
+     locationSearch   = window.location.search,   // ?q=demo
+     locationHash     = window.location.hash;     // #test
+
+/* ============================================================
+* IE11 Fixed element problems
+* ============================================================ */
+if(isIE11) {
+	document.body.addEventListener("mousewheel", function(event) {
+		event.preventDefault();
+		var weelDelta = event.wheelDelta;
+		var currentOffset = window.pageYOffset;
+		window.scrollTo(0, currentOffset - weelDelta);
+	});
+}
+/* ============================================================
+* Common Script
+* ============================================================ */
+var Common = (function () {
+	function Common() {
+		this.onInit();
+	}
+	
+	/**
+	* 初期化
+	*/
+	Common.prototype.onInit = function () {
+		var _this = this;
+		_this.addAgentClass();
+		_this.initGlobalNav();
+		_this.smoothScroll();
+		_this.jsMediaSNS();
+	}
+
+	/**
+	* userAgent Classes to <html>
+	*/
+	Common.prototype.addAgentClass = function(){
+		if (isTablet) {
+			$('html').addClass('is-tablet');
+		}
+		if (isSmartPhone) {
+			$('html').addClass('is-sp');
+		}
+		if (isPC) {
+			$('html').addClass('is-pc');
+		}
+		if (isMobile) {
+			$('html').addClass('is-mobile');
+		}
+		if (isIE) {
+			$('html').addClass('is-ie');
+		}
+		if (isIE11) {
+			$('html').addClass('is-ie11');
+		}
+	}
+
+	/**
+	* smoothScroll
+	*/
+	Common.prototype.smoothScroll = function(){
+		$('body').on('click', 'a[href^="#"]:not([href="#top"])',function(){
+			var href= $(this).attr('href');
+			var target = $(href === '#' || href === '' ? 'html' : href);
+			var position = target.offset().top - 80;
+			$('body,html').animate({scrollTop:position}, 500, 'swing');
+			return false;
+		});
+	}
+
+	/**
+	* グローバリゼーション
+	*/
+	Common.prototype.initGlobalNav = function () {
+	}
+
+	/**
+	* jsFullBackground
+	*/
+	Common.prototype.fullBackground = function(){
+		$('.js-fullbg').jsFullBackground();
+	}
+
+	/**
+	* matchHeight
+	*/
+	Common.prototype.jsMatchHeight = function(){
+		$('.js-matchheight').matchHeight();
+		$('.js-matchheight02').matchHeight();
+		var timer = false;
+		$(window).on('resize', function() {
+			if (timer !== false) {
+				clearTimeout(timer);
+			}
+			timer = setTimeout(function() {
+				$('.js-matchheight').matchHeight();
+				$('.js-matchheight02').matchHeight();
+			}, 500);
+		});
+	}
+
+	//Media SNS
+	Common.prototype.jsMediaSNS = function () {
+		/* ------------------------------------------------------------
+		 SNS
+		* ------------------------------------------------------------ */
+		var lang          = $('html').attr('lang');
+		var titleElemText = window.document.title; //jQueryで<head>要素内の情報を取得できないためこの記法
+
+		var txtOriginal   = titleElemText.replace(/\s/g,'');
+		var txt           = encodeURI(txtOriginal);
+		var url           = location.href;
+
+		var hashTags = ""; //Twitterで使用
+
+		// facebook
+		$('.media-sns__facebook').append('<a href="//www.facebook.com/sharer/sharer.php?u=' + url + '" target="_blank" class="brightover"><img src="./img/icon-facebook.svg" alt="Facebook"></a>');
+		// twitter
+		$('.media-sns__twitter').append('<a href="//twitter.com/intent/tweet?url=' + url + '&amp;text=[' + txt + ']&amp;hashtags=' + hashTags + '" target="_blank" class="brightover"><img src="./img/icon-twitter.svg" alt="Twitter"></a>');
+
+	}
+	return Common;
+}());
+
+/* ============================================================
+ * Execute JavaScript when the DOM is fully loaded.
+ * ============================================================ */
+function eventHandler(){
+	var commonJS;
+	commonJS = new Common();
+}
+if(document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
+
+
+/* ============================================================
+ * Plugin
+ * ============================================================ */
+$.fn.jsFullBackground = function(config){
+	var defaults = {
+			position : 'center center',
+			bgsize: 'cover',
+			repeat: 'no-repeat'
+		};
+	var config = $.extend({}, defaults, config);
+	return this.each(function() {
+		var $this = $(this);
+		var $img = $this.children('img').first();
+		if (!$img.length) return false;
+		var src = $img.attr('src');
+		var position = config.position;
+		var bgsize = config.bgsize;
+		var repeat = config.repeat;
+		if ($this.data('position')) {
+			position = $this.data('position');
+		}
+		if ($this.data('bgsize')) {
+			bgsize = $this.data('bgsize');
+		}
+		if ($this.data('repeat')) {
+			repeat = $this.data('repeat');
+		}
+		$this.css({
+			backgroundSize: bgsize,
+			backgroundImage: 'url(' + src + ')',
+			backgroundRepeat: repeat,
+			backgroundPosition: position
+		});
+		$img.hide();
+	});
+}
+
+
+/*
+* jquery-match-height 0.7.2 by @liabru
+* http://brm.io/jquery-match-height/
+* License MIT
+*/
+!function(t){"use strict";"function"==typeof define&&define.amd?define(["jquery"],t):"undefined"!=typeof module&&module.exports?module.exports=t(require("jquery")):t(jQuery)}(function(t){var e=-1,o=-1,n=function(t){return parseFloat(t)||0},a=function(e){var o=1,a=t(e),i=null,r=[];return a.each(function(){var e=t(this),a=e.offset().top-n(e.css("margin-top")),s=r.length>0?r[r.length-1]:null;null===s?r.push(e):Math.floor(Math.abs(i-a))<=o?r[r.length-1]=s.add(e):r.push(e),i=a}),r},i=function(e){var o={
+byRow:!0,property:"height",target:null,remove:!1};return"object"==typeof e?t.extend(o,e):("boolean"==typeof e?o.byRow=e:"remove"===e&&(o.remove=!0),o)},r=t.fn.matchHeight=function(e){var o=i(e);if(o.remove){var n=this;return this.css(o.property,""),t.each(r._groups,function(t,e){e.elements=e.elements.not(n)}),this}return this.length<=1&&!o.target?this:(r._groups.push({elements:this,options:o}),r._apply(this,o),this)};r.version="0.7.2",r._groups=[],r._throttle=80,r._maintainScroll=!1,r._beforeUpdate=null,
+r._afterUpdate=null,r._rows=a,r._parse=n,r._parseOptions=i,r._apply=function(e,o){var s=i(o),h=t(e),l=[h],c=t(window).scrollTop(),p=t("html").outerHeight(!0),u=h.parents().filter(":hidden");return u.each(function(){var e=t(this);e.data("style-cache",e.attr("style"))}),u.css("display","block"),s.byRow&&!s.target&&(h.each(function(){var e=t(this),o=e.css("display");"inline-block"!==o&&"flex"!==o&&"inline-flex"!==o&&(o="block"),e.data("style-cache",e.attr("style")),e.css({display:o,"padding-top":"0",
+"padding-bottom":"0","margin-top":"0","margin-bottom":"0","border-top-width":"0","border-bottom-width":"0",height:"100px",overflow:"hidden"})}),l=a(h),h.each(function(){var e=t(this);e.attr("style",e.data("style-cache")||"")})),t.each(l,function(e,o){var a=t(o),i=0;if(s.target)i=s.target.outerHeight(!1);else{if(s.byRow&&a.length<=1)return void a.css(s.property,"");a.each(function(){var e=t(this),o=e.attr("style"),n=e.css("display");"inline-block"!==n&&"flex"!==n&&"inline-flex"!==n&&(n="block");var a={
+display:n};a[s.property]="",e.css(a),e.outerHeight(!1)>i&&(i=e.outerHeight(!1)),o?e.attr("style",o):e.css("display","")})}a.each(function(){var e=t(this),o=0;s.target&&e.is(s.target)||("border-box"!==e.css("box-sizing")&&(o+=n(e.css("border-top-width"))+n(e.css("border-bottom-width")),o+=n(e.css("padding-top"))+n(e.css("padding-bottom"))),e.css(s.property,i-o+"px"))})}),u.each(function(){var e=t(this);e.attr("style",e.data("style-cache")||null)}),r._maintainScroll&&t(window).scrollTop(c/p*t("html").outerHeight(!0)),
+this},r._applyDataApi=function(){var e={};t("[data-match-height], [data-mh]").each(function(){var o=t(this),n=o.attr("data-mh")||o.attr("data-match-height");n in e?e[n]=e[n].add(o):e[n]=o}),t.each(e,function(){this.matchHeight(!0)})};var s=function(e){r._beforeUpdate&&r._beforeUpdate(e,r._groups),t.each(r._groups,function(){r._apply(this.elements,this.options)}),r._afterUpdate&&r._afterUpdate(e,r._groups)};r._update=function(n,a){if(a&&"resize"===a.type){var i=t(window).width();if(i===e)return;e=i;
+}n?o===-1&&(o=setTimeout(function(){s(a),o=-1},r._throttle)):s(a)},t(r._applyDataApi);var h=t.fn.on?"on":"bind";t(window)[h]("load",function(t){r._update(!1,t)}),t(window)[h]("resize orientationchange",function(t){r._update(!0,t)})});
